c1863a45217f890ec6d5ec26d2c4ba4d080e2cd3
Netacci <52791481+Netacci@users.noreply.github.com>
2020-03-24 07:49:21 +0100

    Update courses.js

--- courses.js
+++ courses.js
@@ -3,7 +3,5 @@ const myCourses = ['HTML', 'CSS', 'JAVASCRIPT', 'DESIGN'];
 console.log(myName);
 console.log(myCourses);
 for (let i = 0; i <= 200; i++) {
-  if (i % 2 === 0) {
-    console.log(i)
-  };
-};
+  if (i % 2 === 0) console.log(i);
+}
